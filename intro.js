63228e9dc5dd4de2d5fe845eb6d5beb00f25c54f
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-04 11:46:47 +0530

    Add intro.js

---
+++ intro.js
@@ -0,0 +1,8 @@
+// What is OOps?
+/*
+Object Oriented Programming (OOP) is a programing paradigm based on the concept of objects.
+Objects are instance of classes, which can hold both data (atributes/properties) and functions (methods) that operates on the data.
+OOp in JavaScript aims to increase modularity,reusability,and maintainbilty of code 
+*/
+
+// Why OOps?
