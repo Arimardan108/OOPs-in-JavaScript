549808efd455e9b932f40dbd85fe560a62afd015
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-05 07:37:19 +0530

    Update index.js

--- intro.js
+++ intro.js
@@ -2,4 +2,10 @@
 /*
 The main aimof oops is to bind toogather the data and the functions that operate on them so that other part of the code can acess this data except this function 
 */
+
 // Why OOps?
+/*
+Object-Oriented Programming (OOP) enhances code organization, reusability, and maintainability by structuring programs around objects and their interactions. It helps manage complex software systems more efficiently by promoting modularity, making it easier to develop, debug, and scale applications.
+*/
+
+//
