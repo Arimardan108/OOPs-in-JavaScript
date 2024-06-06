c76803e8ef823185c6132c998a18ecc715cf01a6
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-06 15:03:36 +0530

    Update Constructir.js

--- constructor.js
+++ constructor.js
@@ -1,1 +1,7 @@
-// constructor
+function BankAccount(custumerName, balance = 0) {
+  this.custumerName = custumerName;
+  this.accountNumber = Date.now();
+  this.balance = balance;
+}
+const rakeshAccount = new BankAccount("Rakesh k", 10000);
+console.log(rakeshAccount);
