57c19e50c1974015a697a5c72ae8614555016a47
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-08 16:01:24 +0530

    Update file

--- Prototype.js
+++ Prototype.js
@@ -1,1 +1,3 @@
-// Prototype
+// about prototype
+/* In JavaScript, prototypes are a powerful mechanism that allows objects to inherit properties and methods from other objects. Understanding prototypes is essential for understanding how JavaScript's inheritance model works.
+ */
