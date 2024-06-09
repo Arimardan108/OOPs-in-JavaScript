55079d3b2c5bb827b2252e34e40e5c3148bdf0a2
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-09 20:55:07 +0530

    Update file

--- Prototype.js
+++ Prototype.js
@@ -9,23 +9,26 @@ function BankAccount(customerName, balance = 0) {
   this.accountNumber = Date.now();
   this.balance = balance;
 
-  this.deposite = function (amount) {
-    this.balance = amount;
-  };
-  this.withdraw = (amount) => {
-    this.balance -= amount;
-  };
+  // this.deposite = function (amount) {
+  //   this.balance = amount;
+  // };
+  // this.withdraw = (amount) => {
+  //   this.balance -= amount;
+  // };
 }
 
 const rakeshAccount = new BankAccount("Rakesh k");
-// const johnAccount = new BankAccount("johnAccpunt");
+const johnAccount = new BankAccount("johnAccpunt");
 
 // console.log(rakeshAccount, johnAccount);
 BankAccount.prototype.deposite = function (amount) {
   this.balance = amount;
 };
+
 // console.log(BankAccount.prototype);
 
 rakeshAccount.deposite(3000);
+johnAccount.deposite(5000);
 
 console.log(rakeshAccount);
+console.log(johnAccount);
