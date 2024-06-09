13b421a89a533fcc36116993354f0b792f13562e
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-09 21:34:50 +0530

    Update file

--- Classes.js
+++ Classes.js
@@ -4,3 +4,16 @@ classes are the special type of functions.
 We can define the class just like function declarations and function expressions.
 The JavaScript class contains various class members within a body including methods or constructor
 */
+
+class BankAccount {
+  customerName;
+  accountNumber;
+  balance = 0;
+
+  constructor(customerName, balance) {
+    this.customerName = customerName;
+    this.accountNumber = Date.now();
+  }
+}
+
+const rakeshAccount = new BankAccount("Rakesh K", 1000);
