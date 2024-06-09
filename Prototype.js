6baac0a1df0723725f796cb28647497816196d24
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-09 13:22:05 +0530

    Update file

--- Prototype.js
+++ Prototype.js
@@ -3,3 +3,19 @@
 In JavaScript, prototypes are a powerful mechanism that allows objects to inherit properties and methods from other objects.
 Understanding prototypes is essential for understanding how JavaScript's inheritance model works.
 */
+
+function BankAccount(customerName, balance = 0) {
+  this.customerName = customerName;
+  this.accountNumber = this.accountNumber;
+  this.balance = balance;
+
+  this.deposite = function (amount) {
+    this.balance = amount;
+  };
+  this.withdraw = (amount) => {
+    this.balance -= amount;
+  };
+}
+
+const rakeshAccount = new BankAccount("Rakesh k");
+console.log(rakeshAccount);
