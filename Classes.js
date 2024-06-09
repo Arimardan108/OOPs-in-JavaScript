cb3e06310e50387ea515bab3055ab44b3e90c08a
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-09 21:24:03 +0530

    Update file

--- Classes.js
+++ Classes.js
@@ -1,2 +1,6 @@
 // classes
-//
+/*
+classes are the special type of functions.
+We can define the class just like function declarations and function expressions.
+The JavaScript class contains various class members within a body including methods or constructor
+*/
