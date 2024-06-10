efa13764262f125ecf5cd27c1e79af3cc47f78d4
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-10 15:33:36 +0530

    Update file

--- Inheritance.js
+++ Inheritance.js
@@ -1,6 +1,6 @@
 // // Inheritance
 // /*
-// inheritance is a fundamental concept where one class (or function constructor in JavaScript) 
+// inheritance is a fundamental concept where one class (or function constructor in JavaScript)
 // inherits the properties and methods of another class.
 // This allows for code reuse and the creation of hierarchical relationships between classes.
 // In JavaScript, inheritance can be achieved using both the classical and prototypal inheritance models.
@@ -49,11 +49,37 @@
 // rakeshAccount.takePersonalLoan(40000);
 // console.log(rakeshAccount);
 
+// classes
+/*
+classes are the special type of functions.
+We can define the class just like function declarations and function expressions.
+The JavaScript class contains various class members within a body including methods or constructor
+*/
 
+class BankAccount {
+  customerName;
+  accountNumber;
+  balance = 0;
 
+  constructor(customerName, balance = 0) {
+    this.customerName = customerName;
+    this.accountNumber = Date.now();
+    this.balance = balance;
+  }
 
+  deposite(amount) {
+    this.balance += amount;
+  }
 
+  withdraw(amount) {
+    this.balance -= amount;
+  }
+}
 
-
-
-
+const rakeshAccount = new BankAccount("Rakesh K", 1000);
+rakeshAccount.deposite(5000);
+const johnAccount = new BankAccount("John");
+johnAccount.deposite(2000);
+johnAccount.withdraw(200);
+console.log(rakeshAccount);
+console.log(johnAccount);
