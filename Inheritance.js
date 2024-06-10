e0e702eaac26a1311ed888fcbe4b022d4ffa3cf8
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-10 09:11:22 +0530

    UPdate file

--- Inheritance.js
+++ Inheritance.js
@@ -1,1 +1,8 @@
 // Inheritance
+/*
+inheritance is a fundamental concept where one class (or function constructor in JavaScript) 
+inherits the properties and methods of another class.
+This allows for code reuse and the creation of hierarchical relationships between classes.
+In JavaScript, inheritance can be achieved using both the classical and prototypal inheritance models.
+Since the introduction of ES6, the class syntax has made it easier to work with inheritance in a way that is more familiar to those who have experience with other OOP languages.
+*/
