ecbbef0cb7605fb1ddaa732147fa3681b96c5e5e
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-10 07:16:00 +0530

    Update file

--- Classes.js
+++ Classes.js
@@ -10,10 +10,12 @@ class BankAccount {
   accountNumber;
   balance = 0;
 
-  constructor(customerName, balance) {
+  constructor(customerName, balance = 0) {
     this.customerName = customerName;
     this.accountNumber = Date.now();
+    this.balance = balance;
   }
 }
 
 const rakeshAccount = new BankAccount("Rakesh K", 1000);
+console.log(rakeshAccount);
