e39967a98c27bd4a7f9762ff5c14f3c98f1fe2f9
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-14 15:33:59 +0530

    Update file

--- Encapsulation.js
+++ Encapsulation.js
@@ -1,1 +1,4 @@
 // Encapsulation
+/*
+Encapsulation in object-oriented programming (OOP) in JavaScript is a concept where the internal state of an object is hidden from the outside world. This is achieved by using private variables and methods, exposing only the necessary parts through public methods. This helps in protecting the data from being directly accessed or modified, ensuring that the object's state is maintained properly. Encapsulation also makes the code more modular and easier to maintain.
+*/
