66c8901129c935c5a1e00e784bebd1652b9f342b
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-15 15:01:35 +0530

    Update file

--- static.js
+++ static.js
@@ -2,3 +2,10 @@
 /*
 Object-Oriented Programming (OOP) in JavaScript is a programming style that uses objects to organize code, utilizing concepts like encapsulation, inheritance, and polymorphism to structure and manage code more efficiently. JavaScript supports OOP through prototypes and classes, allowing for reusable and modular code.
 */
+
+class user {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
